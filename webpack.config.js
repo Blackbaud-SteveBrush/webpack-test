25b621919d7c84328f6dc2d953ad015ba72cdd21
stevebrush <steve.brush@blackbaud.com>
2016-08-18 12:07:25 -0400

    Added plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
+var webpack = require('webpack');
 module.exports = {
+
+  // Webpack starts here...
   entry: './src/index.js',
+
+  // The bundle file is generated and moved to the build location...
   output: {
     path: 'dist',
     filename: 'bundle.js'
   },
+
   module: {
+
+    // Loaders handle file parsing...
     loaders: [
       {
         test: /\.ts$/,
         loader: 'ts-loader'
       }
     ],
-    plugins: []
+
+    // Plugins modify the compiler...
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin()
+    ]
   }
 };
